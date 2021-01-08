5f8189134d166266aae727b6c30ef4499a89cdec
krissh6563-droid <krishnamitws2000@gmail.com>
2021-01-08 12:14:48 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+print(pyautogui.size())
+
+#time duration in which cursor move 
+time_duration =  time.time() + 30
+while time.time()<time_duration:
+    #Co-ordinate for cursur movement
+    pyautogui.moveTo(500,300,duration=2)
+    pyautogui.moveTo(300,500,duration=2)
+    
